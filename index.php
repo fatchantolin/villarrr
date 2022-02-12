0fa0a5c718ac623e058f4d823004bc48ddb153a1
fatchantolin <99541650+fatchantolin@users.noreply.github.com>
2022-02-12 22:38:27 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content-"
+	width=device-width, initial-scale=1.0">
+	<title>Villar</title>
+
+</head>
+<body>
+<?php include_once("index.html"); ?>
+
+</body>
+</html>
